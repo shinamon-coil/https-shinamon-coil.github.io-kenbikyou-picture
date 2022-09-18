34063e9bf5986c402cd2039c96f0c5c02ea660f1
shinamon-coil <shinamoncoil@gmail.com>
2022-09-18 09:07:01 +0900

    [update] Jsonファル読み込み機能、ランキング表示機能を追加。

---
+++ app.js
@@ -0,0 +1,38 @@
+new Vue({
+  el: "#app",
+  data: {
+    articles: get_article_data(),
+    main_image_src: "./data/pictures/000000000001.jpg"
+  },
+  methods: {
+  },
+  computed: {
+  },
+
+})
+
+function get_article_data() {
+  var articles_data = [];
+  $.ajax({
+    tyoe: 'GET',
+    url: './data/organ-data.json',
+    dataType: 'json'
+  })
+    .then(
+      function (json) {
+        for (var i = 0; i < json.length; i++) {
+          articles_data.push({
+            good: json[i].good,
+            id: json[i].id,
+            japanese_name: json[i].japanese_name,
+            update_date: json[i].update_date,
+            posted_date: json[i].posted_date,
+            user_name: json[i].user_name,
+          });
+        }
+
+      }
+
+    );
+  return articles_data
+}

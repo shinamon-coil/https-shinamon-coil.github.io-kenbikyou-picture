75565fd8643c6062fffa3d8e676120925fba8ee2
shinamon-coil <shinamoncoil@gmail.com>
2022-10-09 00:04:39 +0900

    [add] urlのクエリ処理関数を追加。

--- js/common.js
+++ js/common.js
@@ -1,5 +1,10 @@
 function getQuery() {
+  let searchQuery = document.location.search;
 
+  searchQuery = searchQuery.replace('?q=', '').replace(/%22/g, '');
+  return {
+    searchQuery: searchQuery
+  }
 }
 
 function isTrueData() {

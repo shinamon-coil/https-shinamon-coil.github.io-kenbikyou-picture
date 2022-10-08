9453e555e7a1034a091e158267fd4a568b26831c
shinamon-coil <shinamoncoil@gmail.com>
2022-10-09 00:03:49 +0900

    [add] 検索キーワードを表示するようにした。

---
+++ js/search.js
@@ -0,0 +1,26 @@
+Show()
+
+function Show() {
+  $.ajax({
+    type: 'GET',
+    url: './data/organ-data.json',
+    dataType: 'json'
+  })
+    .then(
+      function (json) {
+        const query = getQuery();
+
+        new Vue({
+          el: "#app",
+          data() {
+            return { searchKeyWord: query.searchQuery }
+          },
+          methods: {
+          },
+          computed: {
+          }
+
+        })
+      }
+    );
+}

45e5040be96890c4bb126fc7c5e399bc60e5abf6
shinamon-coil <shinamoncoil@gmail.com>
2022-10-17 23:32:16 +0900

    [update] showAppを分離。

--- js/common.js
+++ js/common.js
@@ -7,6 +7,6 @@ function getQuery() {
   }
 }
 
-function isTrueData() {
+function searchFullText(keyword) {
 
 }
--- js/search.js
+++ js/search.js
@@ -1,24 +1,26 @@
-Show()
+readJson();
 
-function Show() {
+function showApp(params) {
+  const rootComponent = {
+    data() {
+      return { searchKeyWord: getQuery()["searchQuery"] }
+    },
+    methods() {
+    },
+    computed() {
+    }
+  }
+  const app = Vue.createApp(rootComponent);
+  const vm = app.mount('#app');
+}
+
+function readJson() {
   $.ajax({
     type: 'GET',
     url: './data/organ-data.json',
     dataType: 'json'
   })
     .then(
-      function (json) {
-        const rootComponent = {
-          data() {
-            return { searchKeyWord: getQuery()["searchQuery"] }
-          },
-          methods() {
-          },
-          computed() {
-          }
-        }
-        const app = Vue.createApp(rootComponent);
-        const vm = app.mount('#app');
-      }
+      showApp()
     );
 }

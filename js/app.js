b17f8fdba91a4f4a8e1a7e8d8931f2d8cd5e37ca
shinamon-coil <shinamoncoil@gmail.com>
2023-01-08 08:41:48 +0900

    [fix] 英語の間違いを訂正。

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ function ShowPage() {
             articles: articles_data,
             selected: "",
             language: "English",
-            about_site: { "日本語": "日々行っている生物関連の個人開発、簡易実験などを公開しています。良ければ参考にしてください。もちろん生物以外の記事もあります。", "English": "Welcome to my personal web site! I talk about personal developments,simple experiments about organisms,organisms photos,and so on." },
+            about_site: { "日本語": "日々行っている生物関連の個人開発、簡易実験などを公開しています。良ければ参考にしてください。もちろん生物以外の記事もあります。", "English": "Welcome to my personal web site! I talk about my production,simple experiments about organisms,organisms photos,and so on." },
             logo: { "日本語": "生物好きによる日々の小話集", "English": "biology lover life" },
             blog: {
               "日本語": "作ってみた", "English": "Try to"
